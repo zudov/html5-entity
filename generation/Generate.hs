b86187ba3fa5124eef484a0eceea578a73d7000f
Konstantin Zudov <konstantin@anche.no>
2015-02-18 08:07:25 +0200

    Added lookup functions

--- generation/Generate.hs
+++ generation/Generate.hs
@@ -24,24 +24,36 @@ mkModule :: [(String, [Integer])] -> HsModule
 mkModule ents = HsModule noloc (Module "Text.Html5.Entity.Data") Nothing imports (decls ents)
 
 imports :: [HsImportDecl]
-imports = [HsImportDecl noloc (Module "Data.Map") False Nothing 
-                        (Just (False, [ HsIVar $ HsIdent "fromList"
-                                      , HsIAbs $ HsIdent "Map"]))]
+imports = [ HsImportDecl noloc (Module "Data.Map") True (Just $ Module "M")
+                         (Just (False, [ HsIVar $ HsIdent "fromList"
+                                       , HsIAbs $ HsIdent "Map"]))
+          , HsImportDecl noloc (Module "Data.Set") True (Just $ Module "S")
+                         (Just (False, [ HsIVar $ HsIdent "fromList"
+                                       , HsIAbs $ HsIdent "Set"]))]
 
 decls :: [(String, [Integer])] -> [HsDecl]
 decls ents = 
-    [ HsTypeSig noloc [HsIdent "entities"] (HsQualType [] (HsTyVar (HsIdent "Map String [Int]"))) -- This TyVar is invalid but who cares
-    , HsFunBind [HsMatch noloc (HsIdent "entities") [] (HsUnGuardedRhs (mkEntityMap ents)) []]]
+    [ HsTypeSig noloc [HsIdent "entityMap"] (HsQualType [] (HsTyVar (HsIdent "M.Map String [Int]"))) -- This TyVar is invalid but who cares
+    , HsFunBind [HsMatch noloc (HsIdent "entityMap") [] (HsUnGuardedRhs (mkEntityMap ents)) []]
+
+    , HsTypeSig noloc [HsIdent "entitySet"] (HsQualType [] (HsTyVar (HsIdent "S.Set String"))) -- This TyVar is invalid but who cares
+    , HsFunBind [HsMatch noloc (HsIdent "entitySet") [] (HsUnGuardedRhs (mkEntitySet ents)) []]
+    ]
 
 mkEntityMap :: [(String, [Integer])] -> HsExp
-mkEntityMap ents =
-    HsApp (HsVar $ UnQual $ HsIdent "fromList") (HsList $ map mkMapElemTup ents)
+mkEntityMap ents = HsApp (HsVar $ Qual (Module "M") $ HsIdent "fromList")
+                         (HsList $ map mkMapElemTup ents)
 
 mkMapElemTup :: (String, [Integer]) -> HsExp
 mkMapElemTup (name, codes) =
     HsTuple [ HsLit $ HsString name
             , HsList $ map (HsLit . HsInt) codes
             ] 
+
+mkEntitySet :: [(String, [Integer])] -> HsExp
+mkEntitySet ents = HsApp (HsVar $ Qual (Module "S") $ HsIdent "fromList")
+                         (HsList $ map (HsLit . HsString . fst) ents)
+
 
 noloc :: SrcLoc
 noloc = SrcLoc "unknown" 0 0
--- src/Text/Html5/Entity.hs
+++ src/Text/Html5/Entity.hs
@@ -1,1 +1,49 @@
-module Text.Html5.Entity where
+module Text.Html5.Entity
+    ( -- * Entity lookup
+      entityCodePoints
+    , entityChars
+      -- ** Entity validation
+    , isValidEntity
+      -- * TODO: Entity named lookup
+    )where
+
+import           Control.Applicative ((<$>))
+import           Data.Char (chr)
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+import Text.Html5.Entity.Data
+
+-- | Given an entity looks up unicode code points that represent it.
+--
+-- >>> lookupEntityCodePoints "&mu;"
+-- Just [966]
+--
+-- >>> lookupEntityCodePoints "&nano;"
+-- Nothing
+entityCodePoints :: String -> Maybe [Int]
+entityCodePoints ent = M.lookup ent entityMap
+
+-- | Given an entity returns it's textual representation (if any).
+--
+-- >>> lookupEntityChars "&mu;"
+-- Just "μ"
+-- 
+-- >>> lookupEntityChars "&nano;"
+-- Nothing
+entityChars :: String -> Maybe String
+entityChars ent = map chr <$> M.lookup ent entityMap
+
+-- | Check if entity is valid (if such entity exists).
+--
+-- >>> isValidEntity "&mu;"
+-- True
+--
+-- >>> isValidEntity "&nano;"
+-- False
+isValidEntity :: String -> Bool
+isValidEntity ent = S.member ent entitySet
+
+-- | Given a name of entity looks up code points that represent it.
+{-entityNameCodePoints :: String -> Maybe String-}
+{-entityNameCodePoints = undefined-}
