2bf56405058918fdb50db95c978c065b6381b115
Konstantin Zudov <konstantin@anche.no>
2015-04-10 19:29:58 +0300

    Revert addition of unordered containers
    this feature is branched now
    
    This reverts commit 96e285b069fa6e70acde22efa8b38c66604ce908.

--- generation/Template.hs
+++ generation/Template.hs
@@ -1,11 +1,11 @@
 module Text.Html.Entity.Data where
 
-import           Data.HashMap.Lazy (HashMap)
-import qualified Data.HashMap.Lazy as M
-import           Data.HashSet (HashSet)
-import qualified Data.HashSet as S
+import           Data.Map (Map)
+import qualified Data.Map as M
+import           Data.Set (Set)
+import qualified Data.Set as S
 
 import           Data.Text (Text, pack)
 
-entityMap :: HashMap Text [Int]
-entitySet :: HashSet Text
+entityMap :: Map Text [Int]
+entitySet :: Set Text
--- src/Text/Html/Entity/Data.hs
+++ src/Text/Html/Entity/Data.hs
@@ -1,13 +1,13 @@
 module Text.Html.Entity.Data where
-import Data.HashMap.Lazy (HashMap)
-import qualified Data.HashMap.Lazy as M
-import Data.HashSet (HashSet)
-import qualified Data.HashSet as S
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.Set (Set)
+import qualified Data.Set as S
 import Data.Text (Text, pack)
  
-entityMap :: HashMap Text [Int]
+entityMap :: Map Text [Int]
  
-entitySet :: HashSet Text
+entitySet :: Set Text
 entityMap
   = M.fromList
       [(pack "&intlarhk;", [10775]), (pack "&male;", [9794]),
